0b9ee08d07cce881479ace9d4d9772e415def89f
Sebastian Aigner <sebastian.aigner@tum.de>
2019-07-25 21:30:24 +0200

    drastically reduce implementation complexity

--- src/main/kotlin/Matchers.kt
+++ src/main/kotlin/Matchers.kt
@@ -9,6 +9,15 @@ fun matchByRegex(remaining: String, regex: Regex, out: (String) -> Token): Pair<
     return Pair(token, parsed)
 }
 
+fun matchMultipleByRegex(remaining: String, regex: Regex, out: (MatchResult) -> Token): Pair<Token?, String> {
+    var token: Token? = null
+    val parsed = remaining.replace(regex) {
+        token = out(it)
+        ""
+    }
+    return Pair(token, parsed)
+}
+
 tailrec fun parseByMatchers(matchers: List<Matcher>, string: String, parsed: List<Token> = emptyList()): List<Token> {
     if (string.count() == 0) return parsed
 
@@ -20,10 +29,48 @@ tailrec fun parseByMatchers(matchers: List<Matcher>, string: String, parsed: Lis
     return parseByMatchers(matchers, string.drop(1), parsed + Token.Literal(string.first().toString()))
 }
 
-fun matchIssue(remaining: String) = matchByRegex(remaining, "^(\\w+-\\d+)".toRegex()) { Token.Issue(it) }
-fun matchEscaped(remaining: String) = matchByRegex(remaining, "^<([^>]+)>".toRegex()) { Token.Escaped(it) }
+fun matchIssue(remaining: String) = matchByRegex(remaining, "^(\\w+-\\d+)".toSafeRegex()) { Token.Issue(it) }
 
+fun matchMarkdownLink(remaining: String) =
+    matchMultipleByRegex(remaining, "^\\[([^]]+)]\\(<(https?[^)]+)>\\)".toSafeRegex()) {
+    Token.MarkdownLink(it.groupValues[1], it.groupValues[2])
+}
+
+fun matchEscaped(remaining: String) = matchByRegex(remaining, "^<([^>]+)>".toSafeRegex()) {
+    distinguishEscaped(Token.Escaped(it))
+}
+
+fun distinguishEscaped(t: Token.Escaped): Token {
+    return  listOf(
+            escapedYouTrackLink(t),
+            escapedPerson(t)
+    ).filterNotNull().firstOrNull() ?: fallbackEscapedLink(t)
+}
 
 fun String.parsedBy(m: List<Matcher>): List<Token> {
     return parseByMatchers(m, this)
+}
+
+fun escapedYouTrackLink(t: Token.Escaped): Token? {
+    val youTrackRegex = "issue/(\\w+-\\d+)".toRegex()
+    val matches = youTrackRegex.find(t.content)
+    return matches?.groupValues?.get(1)?.let { Token.Issue(it) }
+}
+
+fun escapedPerson(t: Token.Escaped): Token? {
+    val personRegex = "@(\\w+)\\|.*".toRegex()
+    val matches = personRegex.find(t.content)
+    return matches?.groupValues?.get(1)?.let { Token.Person(it) }
+}
+
+fun fallbackEscapedLink(t: Token.Escaped): Token {
+    val link = "://".toRegex()
+    if (link.containsMatchIn(t.content)) {
+        return Token.Link(t.content)
+    }
+    return t
+}
+
+fun String.toSafeRegex(): Regex {
+    if(this.startsWith("^")) return this.toRegex() else error("When consuming tokens, only accept the first character!")
 }
--- src/main/kotlin/SlackParser.kt
+++ src/main/kotlin/SlackParser.kt
@@ -1,39 +1,39 @@
 
 
-sealed class Token(val content: String) {
-    fun debugString(): String {
-        return this.javaClass.name + "[$content]"
+sealed class Token() {
+    abstract fun out(): String
+
+    class Literal(val content: String) : Token() {
+        override fun out(): String = content
     }
-    open fun out() = content
+    class Escaped(val content: String) : Token() {
+        override fun out() = content
+    }
 
-    class Literal(content: String) : Token(content)
-    class Escaped(content: String) : Token(content)
-
-    class Link(content: String) : Token(content) {
+    class Link(val content: String) : Token() {
         override fun out() = "<link '$content'/>"
     }
 
-    class Issue(content: String) : Token(content) {
+    class MarkdownLink(val name: String, val link: String): Token() {
+        override fun out() = "<link $name @ $link>"
+    }
+
+    class Issue(val content: String) : Token() {
         override fun out() = "<issue '$content'/>"
     }
 
-    class Person(content: String) : Token(content) {
+    class Person(val content: String) : Token() {
         override fun out() = "<person '$content'/>"
     }
 }
 
 
 fun main() {
-    val matcherCollection = listOf<Matcher>(::matchIssue, ::matchEscaped)
-    val transformers = listOf(::linkTransform, ::youtrackLinkTransform, ::personTransform)
+    val matcherCollection = listOf<Matcher>(::matchIssue, ::matchMarkdownLink, ::matchEscaped)
 
     val input =
-    "<http://link.com> <@UGLDAJJ92|sebastian.aigner620> ABCD-1234 <https://youtrack.jetbrains.com/issue/ABCD-1234> abc <yikes>"
+    "<http://link.com> [title](<https://oof.com>) <@UGLDAJJ92|sebastian.aigner620> ABCD-1234 <https://youtrack.jetbrains.com/issue/ABCD-1234> abc <yikes>"
     val parsed = input.parsedBy(matcherCollection)
-    val final = parsed.transformedBy(transformers)
-
-    println(input)
-    println(final.joinToString { it.debugString() })
-    println(final.joinToString(separator = "") { it.out() })
+    println(parsed.joinToString(separator = "") { it.out() })
 }
 
